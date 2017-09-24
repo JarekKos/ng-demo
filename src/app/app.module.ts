074066abe401ccaf8d8b1522a56b0fe709d9a7d9
jarekk <j.kosterkiewicz@gmail.com>
2017-09-24 12:21:45 +0200

    * enabling ReactiveFormModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';  // <-- #1 import module
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,6 +28,7 @@ import { StepDirective } from './step.directive';
     BrowserModule.withServerTransition({appId: 'ng-demo-pre'}),
     AppRoutingModule,
     HttpModule,
+    ReactiveFormsModule
   ],
   providers: [TodoService],
   bootstrap: [AppComponent],
