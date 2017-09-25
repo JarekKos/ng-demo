7ee88597caa4f3e54e3ec7082dd5797ffbb436b3
jarekk <j.kosterkiewicz@gmail.com>
2017-09-25 22:08:21 +0200

    * further works(2)

--- src/app/reactive-form/company-data/company-data.component.ts
+++ src/app/reactive-form/company-data/company-data.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+import { StepInterface } from '../interfaces/StepInterface';
 
 @Component({
   selector: 'app-company-data',
   templateUrl: './company-data.component.html',
   styleUrls: ['./company-data.component.css']
 })
-export class CompanyDataComponent implements OnInit {
+export class CompanyDataComponent implements OnInit, StepInterface {
+
+  form: FormGroup;
+  @Output() onClickButton = new EventEmitter<number>();
 
   constructor() { }
 
