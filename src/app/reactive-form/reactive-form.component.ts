09dd1f524caeae5a748ab91c1ee3024baaaa7bb4
jarekk <j.kosterkiewicz@gmail.com>
2017-09-26 21:05:26 +0200

    * update to ReactiveForm component

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -4,6 +4,7 @@ import { StepDirective } from '../step.directive';
 import { PersonalDataComponent } from './components/personal-data/personal-data.component';
 import { CompanyDataComponent } from './components/company-data/company-data.component';
 import { UserService } from './services/UserService';
+import { SummaryComponent } from './components/summary/summary.component';
 
 @Component({
   selector: 'app-reactive-form',
@@ -15,7 +16,8 @@ export class ReactiveFormComponent implements OnInit, AfterViewInit {
 
   steps: Array<{step, label, component}> = [
     {step: 1, label: 'Step 1', component: PersonalDataComponent},
-    {step: 2, label: 'Step 2', component: CompanyDataComponent}
+    {step: 2, label: 'Step 2', component: CompanyDataComponent},
+    {step: 3, label: 'Summary', component: SummaryComponent}
   ];
   activeTab: {step, label, component};
   stepsContainer: ViewContainerRef = null;
@@ -24,7 +26,10 @@ export class ReactiveFormComponent implements OnInit, AfterViewInit {
   // get element of StepDirective
   @ViewChild(StepDirective) stepHost: StepDirective;
 
-  constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
     this.activeTab = this.steps[0];
@@ -42,6 +47,11 @@ export class ReactiveFormComponent implements OnInit, AfterViewInit {
   }
 
   setActive(index: number) {
+    if (this.cmp && this.cmp['form'] && this.cmp['form'].valid) {
+      this.userService.updateUser(this.cmp['form'].value);
+      console.log(this.userService.getUser());
+    }
+
     if (this.cmp === null || this.activeTab.step > this.steps[index].step || this.cmp['form'].valid) {
       this.stepsContainer.clear();
 
@@ -51,7 +61,9 @@ export class ReactiveFormComponent implements OnInit, AfterViewInit {
       const ref = this.stepsContainer.createComponent(displayedStep);
       this.cmp = ref.instance;
 
-      this.cmp['onClickButton'].subscribe(slideNumber => this.setActive(slideNumber));
+      if (this.cmp['onClickButton']) {
+        this.cmp['onClickButton'].subscribe(slideNumber => this.setActive(slideNumber));
+      }
     }
   }
 }
