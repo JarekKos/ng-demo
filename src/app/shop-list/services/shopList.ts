f058799980ceeaea147b91e5625135030cc426a0
jarekk <j.kosterkiewicz@gmail.com>
2017-10-03 21:01:46 +0200

    * update to ShopList service

--- src/app/shop-list/services/shopList.ts
+++ src/app/shop-list/services/shopList.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
+// import 'rxjs/add/observable/filter';
 
 @Injectable()
 export class ShopList {
   list = [
-    'cheese',
-    'egs',
-    'bred',
-    'carrots',
-    'salad',
+    {id: 1, name: 'cheese'},
+    {id: 2, name: 'egs'},
+    {id: 3, name: 'bred'},
+    {id: 4, name: 'carrots'},
+    {id: 5, name: 'salad'},
   ];
 
   getList() {
